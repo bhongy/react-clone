932622531d7b4f7bfdd3108db9099e5e2e163f95
Thanik Bhongbhibhat <email@thanik.me>
2019-06-29 08:34:41 -0700

    Simplify jest config: use defaults

--- packages/scheduler/jest.config.js
+++ packages/scheduler/jest.config.js
@@ -2,10 +2,7 @@
 
 module.exports = {
   rootDir: './src',
-  // `js` is needed for node_modules
-  moduleFileExtensions: ['js', 'ts'],
-  testMatch: ['<rootDir>/**/?(*.)+(test).ts'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.tsx?$': 'ts-jest',
   },
 };
